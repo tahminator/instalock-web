ca1f5e381defc767c86e605cc53e48ddba6cf499
Tahmid Ahmed <ta@0pengu.in>
2024-04-16 02:05:35 -0400

    m

---
+++ client/src/components/SkeletonMatchCardComponent/SkeletonMatchCardComponent.tsx
@@ -0,0 +1,11 @@
+import { Box, Card, Container, Skeleton } from '@mantine/core';
+
+export default function SkeletonMatchCardComponent() {
+  return (
+    <Box style={{ flexGrow: 1 }}>
+      <Skeleton w="100%" h="416" visible>
+        <Card shadow="xs">hi</Card>
+      </Skeleton>
+    </Box>
+  );
+}
