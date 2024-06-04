87db3ca8120d6b92ade00c82c172dbb0cd447924
Tahmid Ahmed <tahmid@midhat.io>
2024-06-04 10:51:59 -0700

    Added Sentry

--- api/app_maker.py
+++ api/app_maker.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sentry_sdk
 # from celery import Celery
 
 from api.extension import db, login_manager, mail, bcrypt, migrate, sess
@@ -6,9 +7,19 @@ from api.config import DevelopmentConfig, ProductionConfig
 
 
 def create_app():
+    sentry_sdk.init(
+        dsn="https://b7a6b2a49e949208195878e21576d8f7@o4507374881341440.ingest.us.sentry.io/4507374891040768",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
     app = Flask(__name__)
-    # app.config.from_object(DevelopmentConfig)
-    app.config.from_object(ProductionConfig)
+    app.config.from_object(DevelopmentConfig)
+    # app.config.from_object(ProductionConfig)
 
     db.init_app(app)
     login_manager.init_app(app)
