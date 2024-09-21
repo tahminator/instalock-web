82c0dce93f02a48196e447e1d603992da5d4a661
Tahmid Ahmed <tahmid@midhat.io>
2024-09-21 19:07:04 -0400

    Extended urlencoded turned on

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   console.log(
