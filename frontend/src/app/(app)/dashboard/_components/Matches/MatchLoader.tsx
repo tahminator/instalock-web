ea8b9d557b60b641e5311a7cfd0e32805193fc08
Tahmid Ahmed <tahmid@tahmid.io>
2024-12-15 14:50:44 -0500

    Disabled match feature

--- frontend/src/app/(app)/dashboard/_components/Matches/MatchLoader.tsx
+++ frontend/src/app/(app)/dashboard/_components/Matches/MatchLoader.tsx
@@ -1,3 +1,8 @@
 export default function MatchLoader() {
-  return <></>;
+  return (
+    <div className="bg-red-300 border border-white p-4 rounded-full text-black text-center">
+      I'm sorry, but the matches feature is disabled due to the fact that Riot
+      Games released a breaking change. Please check back soon.
+    </div>
+  );
 }
