68a70e18bdec1eec090f339073d5fe03ccfaa555
Tahmid Ahmed <tahmid@tahmid.io>
2024-12-17 15:20:20 -0500

    Make skeleton match card longer

--- frontend/src/app/(app)/dashboard/_components/Matches/MatchCard/SkeletonCard/SkeletonCard.tsx
+++ frontend/src/app/(app)/dashboard/_components/Matches/MatchCard/SkeletonCard/SkeletonCard.tsx
@@ -2,7 +2,7 @@ import { Card, Skeleton } from "@mantine/core";
 
 export default function SkeletonMatchCardComponent() {
   return (
-    <Skeleton className="max-h-96 min-h-72">
+    <Skeleton className="max-h-[30rem] min-h-72">
       <Card shadow="xs">hi</Card>
     </Skeleton>
   );
