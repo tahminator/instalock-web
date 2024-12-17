9deb9fd0cc85de716b526b4c43241a4ae153de5d
Tahmid Ahmed <tahmid@tahmid.io>
2024-12-17 13:32:56 -0500

    Changed the script to use upsert instead of connectOrCreate

--- scripts/cron/helpers/loadMatches.ts
+++ scripts/cron/helpers/loadMatches.ts
@@ -89,9 +89,57 @@ export const loadMatchesForEachUser = async () => {
         where: { id: user.id },
         data: {
           riotMatches: {
-            connectOrCreate: {
+            upsert: {
               where: {
                 id: matchInfo?.matchId ?? randomUUID(),
+              },
+              update: {
+                id: matchInfo?.matchId ?? randomUUID(),
+                mapId: mapUrlToUuidObject[matchInfo?.mapId as MapUrl],
+                gameVersion: matchInfo?.gameVersion,
+                gameStart: new Date(matchInfo?.gameStartMillis ?? ""),
+                gameEnd: new Date(
+                  (matchInfo?.gameLengthMillis ?? 0) +
+                    (matchInfo?.gameLengthMillis ?? 0)
+                ),
+                isCompleted: matchInfo?.isCompleted,
+                queueId: matchInfo?.queueID,
+                isRanked: matchInfo?.isRanked,
+                seasonId: matchInfo?.seasonId,
+                roundsPlayed: teams && teams[0].roundsPlayed,
+                teamWon:
+                  teams &&
+                  (teams[0].teamId === "Red" && teams[0].won === true
+                    ? "Red"
+                    : "Blue"),
+                teamBlueRoundsWon: teamBlue?.roundsWon,
+                teamRedRoundsWon: teamRed?.roundsWon,
+                matchPlayers: {
+                  connectOrCreate: players?.map((player) => ({
+                    where: { id: player.subject }, // Assumes 'id' is unique in the database
+                    create: {
+                      id: player.subject ?? randomUUID(),
+                      riotTag: `${player.gameName}#${player.tagLine}`,
+                      teamId: player.teamId,
+                      characterId: player.characterId,
+                      kills: player.stats?.kills ?? 0,
+                      deaths: player.stats?.deaths ?? 0,
+                      assists: player.stats?.assists ?? 0,
+                      tier: player.competitiveTier,
+                      playerCard: player.playerCard,
+                      playerTitle: player.playerTitle,
+                      teamColor: player.teamId === "Blue" ? "Blue" : "Red",
+                      teamWon:
+                        teams &&
+                        teams.find((team) => team.teamId === player.teamId)
+                          ?.won,
+                      teamRoundsWon:
+                        teams &&
+                        teams.find((team) => team.teamId === player.teamId)
+                          ?.roundsPlayed,
+                    },
+                  })),
+                },
               },
               create: {
                 id: matchInfo?.matchId ?? randomUUID(),
