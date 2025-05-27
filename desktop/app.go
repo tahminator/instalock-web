ce938fc0831aecd7653fe232e6cf9de2632a33a5
Tahmid Ahmed <tahmid@tahmid.io>
2025-05-27 02:22:32 -0400

    Update local url protocol to be https (required)

--- desktop/app.go
+++ desktop/app.go
@@ -246,7 +246,7 @@ func (a *App) FindRank(payload FindRankPayload) *Response {
 
 // TODO - Finish this function.
 func (a *App) LocalAuthenticate() {
-	val, err := token.GrabToken("http://127.0.0.1", a.ctx)
+	val, err := token.GrabToken("https://127.0.0.1", a.ctx)
 
 	data, tErr := json.MarshalIndent(val, "", "  ")
 	if tErr != nil {
