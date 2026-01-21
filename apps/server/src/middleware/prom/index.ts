ed543fb18ea5daf9765d39477a8fc0b403e13e60
Tahmid Ahmed <tahmid@tahmid.io>
2026-01-21 03:56:28 -0500

    improved metric path normalization

--- apps/server/src/middleware/prom/index.ts
+++ apps/server/src/middleware/prom/index.ts
@@ -2,6 +2,28 @@ import { Controller, Middleware } from "@tahminator/sapling";
 import { NextFunction, Request, Response } from "express";
 import promBundle from "express-prom-bundle";
 
+const originalNormalize = promBundle.normalizePath;
+
+const fileExtensions = [
+  "html",
+  "js",
+  "json",
+  "csv",
+  "css",
+  "png",
+  "svg",
+  "eot",
+  "ttf",
+  "woff",
+  "appcache",
+  "jpg",
+  "jpeg",
+  "gif",
+  "ico",
+  "txt",
+  "webp",
+];
+
 @Controller()
 export class PrometheusMiddleware {
   private readonly plugin: ReturnType<typeof promBundle>;
@@ -11,8 +33,19 @@ export class PrometheusMiddleware {
       includeMethod: true,
       includePath: true,
       metricsPath: "/api/metrics",
-      normalizePath: (req, _) => {
-        return req.route?.path ?? "NULL";
+      normalizePath: (req, opts) => {
+        const path = originalNormalize(req, opts);
+
+        if (path.startsWith("/assets/")) {
+          return "/assets/#val";
+        }
+
+        // hacky, but better to group them then fill up my metrics with garbage
+        if (fileExtensions.find((ext) => path.includes(ext))) {
+          return "/assets/static/#val";
+        }
+
+        return path;
       },
       promClient: {
         collectDefaultMetrics: {},
