a70ee0e57c8dd4d62a21bd14e6f110ffc64c4732
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-14 22:29:37 -0400

    add log fix to cron as well

--- apps/scripts/cron/patches.ts
+++ apps/scripts/cron/patches.ts
@@ -1,43 +1,46 @@
 import { pino } from "pino";
 
+import { wrap } from "./logger";
+
 const _oldLog = console.log;
 const _oldWarn = console.warn;
 const _oldError = console.error;
 const _oldDebug = console.debug;
 
-const logger = pino(
-  process.env.NODE_ENV === "development"
-    ? {
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-          },
-        },
-      }
-    : undefined,
-);
+const logger = wrap(pino());
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.log = function (...args: any[]) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (logger.info as any)(...args);
+  const _ =
+    process.env.NODE_ENV === "development"
+      ? _oldLog(...args)
+      : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (logger.info as any)(...args);
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.warn = function (...args: any[]) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (logger.warn as any)(...args);
+  const _ =
+    process.env.NODE_ENV === "development"
+      ? _oldWarn(...args)
+      : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (logger.warn as any)(...args);
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.error = function (...args: any[]) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (logger.error as any)(...args);
+  const _ =
+    process.env.NODE_ENV === "development"
+      ? _oldError(...args)
+      : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (logger.error as any)(...args);
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.debug = function (...args: any[]) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (logger.debug as any)(...args);
+  const _ =
+    process.env.NODE_ENV === "development"
+      ? _oldDebug(...args)
+      : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (logger.debug as any)(...args);
 };
