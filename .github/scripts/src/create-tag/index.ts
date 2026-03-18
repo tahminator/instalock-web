0012b098ecc1fa46574843f008157263737b5627
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-18 03:07:05 -0400

    Use PAT to create tags

--- .github/scripts/src/create-tag/index.ts
+++ .github/scripts/src/create-tag/index.ts
@@ -1,7 +1,26 @@
+import { getEnvVariables } from "@/utils/load-env";
+
 import { createTag } from "../utils/create-tag";
 
 export async function main() {
-  await createTag({});
+  const ciEnv = await getEnvVariables(["ci"]);
+  const { githubPat } = parseCiEnv(ciEnv);
+
+  await createTag({
+    token: githubPat,
+  });
+}
+
+function parseCiEnv(ciEnv: Record<string, string>) {
+  const githubPat = (() => {
+    const v = ciEnv["GITHUB_PAT"];
+    if (!v) {
+      throw new Error("Missing GITHUB_PAT from .env.ci");
+    }
+    return v;
+  })();
+
+  return { githubPat };
 }
 
 main()
